5c9a6576c65120a1c5292c86a76cf8c4121705a1
Rojey <3832942@qq.com>
2014-09-30 02:11:34 +0800

    Md
    md ok

---
+++ structure.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rojey
+ * Date: 14/9/28
+ * Time: 下午11:44
+ */
+
+/**
+ *      user
+ *              id
+ *              email
+ *              nickname
+ *              password
+ *              credit
+ *              income
+ *
+ *      package
+ *              id
+ *              price           价格
+ *              traffic         流量
+ *              limit           时间
+ *
+ *      invoice
+ *              id
+ *              price
+ *              paid_time
+ *              status
+ *
+ *       service
+ *              id
+ *              user_id
+ *              package_id
+ *              invoice_id
+ *              begin_time
+ *              end_time
+ *
+ */
---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rojey
+ * Date: 14/9/29
+ * Time: 上午12:57
+ */
+
+$do = new Fuck();
+
+$do->hello->fucku;
+
+$do->hehe;
